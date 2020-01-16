266f7dd4a6664b8f5c612952069a1d79afc58d2b
xandersingeleverdewerk <47180650+xandersingeleverdewerk@users.noreply.github.com>
2020-01-16 16:25:22 +0100

    Add files via upload

---
+++ Assets/Scripts/PlayTimerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int playtime = 0;
+    private int seconds = 0;
+    private int minutes = 0;
+    private int hours = 0;
+    private int days = 0;
+
+    void Start()
+    {
+        StartCoroutine ("Playtimer");
+    }
+    
+    private IEnumerator Playtime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            playtime += 1;
+            seconds = (playtime % 60);
+            minutes = (playtime / 60) % 60;
+            hours = (playtime / 3600) % 24;
+            days = (playtime / 86400) % 365;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(50,50, 400,50), "Playtime = " + days.ToString() + " Days" + hours.ToString() + " Hours" + minutes.ToString() + " Minutes" + seconds.ToString() + " seconds");
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
